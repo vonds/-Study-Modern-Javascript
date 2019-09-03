1bb53b0b3049607604b423b4e4d7f15f56d611b3
Vonds <vonds13@gmail.com>
2019-09-03 10:18:21 -0400

    number guesser study 2

--- Modern Javascript/DOM/numberGuesser/app.js
+++ Modern Javascript/DOM/numberGuesser/app.js
@@ -1,77 +1,214 @@
+const getRandomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 // Game values
 let min = 1,
     max = 10,
     winningNum = getRandomNum(min, max),
     guessesLeft = 3
-// UI Elements
+
+// UI Elements 
 const game = document.querySelector('#game'),
     minNum = document.querySelector('.min-num'),
     maxNum = document.querySelector('.max-num'),
     guessBtn = document.querySelector('#guess-btn'),
     guessInput = document.querySelector('#guess-input'),
     message = document.querySelector('.message')
-// Assign Ui min and max
+
+// Assign UI min and max
 minNum.textContent = min
 maxNum.textContent = max
 
-// Play again event listener
-game.addEventListener('mousedown', e => {
-    if (e.target.className === 'play-again') {
-        window.location.reload()
-    }
-})
-
 // Set message
 const setMessage = (msg, color) => {
-    message.style.color = color
     message.textContent = msg
+    message.style.color = color
 }
 
+
 const gameOver = (won, msg) => {
-    let color = won === true ? color = 'green' : color = 'red'
-    // Disable input
+    let color
+    won === true ? color = 'green' : color = 'red'
     guessInput.disabled = true
-    // Change border color
     guessInput.style.borderColor = color
-    // Set text color 
-    message.style.color = color
-    // Set message
-    setMessage(msg)
-    // Play Again?
+    setMessage(msg, color)
+
     guessBtn.value = 'Play Again'
     guessBtn.className += 'play-again'
 }
 
-// Get winning number
-const getRandomNum = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
+
 // Listen for guess
 guessBtn.addEventListener('click', e => {
     let guess = parseInt(guessInput.value)
-
-    // Validate input
+    // Validate
     if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`)
+        setMessage(`please enter a number between ${min} and ${max}`, 'red')
     }
+
     // Check if won
     if (guess === winningNum) {
-        gameOver(true, `${winningNum} is correct, YOU WIN!`)
+        // Game over - won
+        gameOver(true, `${winningNum} is correct, YOU WIN`)
     } else {
         // Wrong number
         guessesLeft -= 1
         if (guessesLeft === 0) {
+            // Game over - lost
             gameOver(false, `Game over, you lost. The correct number was ${winningNum}`)
-
         } else {
             // Game continues - answer wrong
-
-            // Change border color
-            guessInput.style.borderColor = 'red'
-            // Clear input 
+            // Clear input
             guessInput.value = ''
             // Tell user its the wrong number
             setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red')
         }
+    }
+})
+
+// 1
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 2
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 3
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 4
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 5
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 6
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 7
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 8
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 9
+guessBtn.addEventListener('clikc', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+// 10
+guessBtn.addEventListener('click', e => {
+    let guess = parseInt(guessInput.value)
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    if (guess === winningNum) {
+
+    } else {
+
+    }
+})
+
+game.addEventListener('mousedown', e => {
+    if (e.target.className === 'play-again') {
+        window.location.reload()
     }
 })
