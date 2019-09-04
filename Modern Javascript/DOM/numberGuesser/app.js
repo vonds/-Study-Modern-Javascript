84fda7769e0805dccc04296af06cddfb0cbce7d9
Vonds <vonds13@gmail.com>
2019-09-03 21:48:16 -0400

    number guesser study 5

--- Modern Javascript/DOM/numberGuesser/app.js
+++ Modern Javascript/DOM/numberGuesser/app.js
@@ -26,7 +26,6 @@ const setMessage = (msg, color) => {
     message.style.color = color
 }
 
-
 const gameOver = (won, msg) => {
     let color
     won === true ? color = 'green' : color = 'red'
@@ -39,173 +38,37 @@ const gameOver = (won, msg) => {
 }
 
 
-// Listen for guess
+// 10
+const gameOver = (won, msg) => {
+    let color
+    won === true ? color = 'green' : color = 'red'
+    guessInput.disabled = true
+    guessInput.style.borderColor = color
+    setMessage(msg, color)
+
+    guessBtn.value = 'Play Again'
+    guessBtn.className += 'play-again'
+}
+
 guessBtn.addEventListener('click', e => {
     let guess = parseInt(guessInput.value)
-    // Validate
     if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`please enter a number between ${min} and ${max}`, 'red')
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
     }
 
-    // Check if won
     if (guess === winningNum) {
-        // Game over - won
-        gameOver(true, `${winningNum} is correct, YOU WIN`)
+        gameOver(true, `${winningNum} is correct, YOU WIN!`)
     } else {
-        // Wrong number
         guessesLeft -= 1
         if (guessesLeft === 0) {
-            // Game over - lost
             gameOver(false, `Game over, you lost. The correct number was ${winningNum}`)
         } else {
-            // Game continues - answer wrong
-            // Clear input
             guessInput.value = ''
-            // Tell user its the wrong number
             setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red')
         }
     }
 })
 
-// 1
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 2
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 3
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 4
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 5
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 6
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 7
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 8
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 9
-guessBtn.addEventListener('clikc', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
-
-// 10
-guessBtn.addEventListener('click', e => {
-    let guess = parseInt(guessInput.value)
-    if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-    }
-
-    if (guess === winningNum) {
-
-    } else {
-
-    }
-})
 
 game.addEventListener('mousedown', e => {
     if (e.target.className === 'play-again') {
