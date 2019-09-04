20c0cf3d1a734f555295aca507e11ccfc877c81c
Vonds <vonds13@gmail.com>
2019-09-04 10:11:27 -0400

    number guesser study 2

--- Modern Javascript/DOM/numberGuesser/app.js
+++ Modern Javascript/DOM/numberGuesser/app.js
@@ -2,25 +2,21 @@ const getRandomNum = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-// Game values
 let min = 1,
     max = 10,
     winningNum = getRandomNum(min, max),
     guessesLeft = 3
 
-// UI Elements 
 const game = document.querySelector('#game'),
     minNum = document.querySelector('.min-num'),
     maxNum = document.querySelector('.max-num'),
-    guessBtn = document.querySelector('#guess-btn'),
     guessInput = document.querySelector('#guess-input'),
+    guessBtn = document.querySelector('#guess-btn'),
     message = document.querySelector('.message')
 
-// Assign UI min and max
 minNum.textContent = min
 maxNum.textContent = max
 
-// Set message
 const setMessage = (msg, color) => {
     message.textContent = msg
     message.style.color = color
@@ -37,23 +33,11 @@ const gameOver = (won, msg) => {
     guessBtn.className += 'play-again'
 }
 
-
-// 10
-const gameOver = (won, msg) => {
-    let color
-    won === true ? color = 'green' : color = 'red'
-    guessInput.disabled = true
-    guessInput.style.borderColor = color
-    setMessage(msg, color)
-
-    guessBtn.value = 'Play Again'
-    guessBtn.className += 'play-again'
-}
-
-guessBtn.addEventListener('click', e => {
+guessBtn.addEventListener('click', () => {
     let guess = parseInt(guessInput.value)
+
     if (isNaN(guess) || guess < min || guess > max) {
-        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+        return setMessage(`Please enter a number betweem ${min} and ${max}`, 'red')
     }
 
     if (guess === winningNum) {
@@ -61,14 +45,13 @@ guessBtn.addEventListener('click', e => {
     } else {
         guessesLeft -= 1
         if (guessesLeft === 0) {
-            gameOver(false, `Game over, you lost. The correct number was ${winningNum}`)
+            gameOver(false, `Game over, you lost. The correct number is ${winningNum}`)
         } else {
             guessInput.value = ''
             setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red')
         }
     }
 })
-
 
 game.addEventListener('mousedown', e => {
     if (e.target.className === 'play-again') {
