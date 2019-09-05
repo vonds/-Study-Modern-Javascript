fd9b16fa5249709aea86e7d67db482b3f453091f
Vonds <vonds13@gmail.com>
2019-09-05 09:34:21 -0400

    loan calculator study 8

--- Modern Javascript/DOM/loanCalculator/app.js
+++ Modern Javascript/DOM/loanCalculator/app.js
@@ -4,21 +4,21 @@ const calculateResults = () => {
     const amount = document.querySelector('#amount')
     const interest = document.querySelector('#interest')
     const years = document.querySelector('#years')
-    const monthlyPayments = document.querySelector('#monthly-payment')
-    const totalInterest = document.querySelector('#total-interest')
+    const monthlyPayment = document.querySelector('#monthly-payment')
     const totalPayment = document.querySelector('#total-payment')
+    const totalInterest = document.querySelector('#total-interest')
 
     const principal = parseFloat(amount.value)
     const calculatedInterest = parseFloat(interest.value) / 100 / 12
-    const calculatedPayments = parseFloat(years.value) * 12
+    const calculatedPayment = parseFloat(years.value) * 12
 
-    const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+    const x = Math.pow(1 + calculatedInterest, calculatedPayment)
     const monthly = (principal * x * calculatedInterest) / (x - 1)
 
     if (isFinite(monthly)) {
-        monthlyPayments.value = monthly.toFixed(2)
-        totalPayment.value = (monthly * calculatedPayments).toFixed(2)
-        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+        monthlyPayment.value = monthly.toFixed(2)
+        totalPayment.value = (monthly * calculatedPayment).toFixed(2)
+        totalInterest.value = ((monthly * calculatedPayment) - principal).toFixed(2)
         document.querySelector('#results').style.display = 'block'
         document.querySelector('#loading').style.display = 'none'
     } else {
@@ -37,7 +37,7 @@ const errorMessage = error => {
 }
 
 const clearError = () => {
-    document.querySelector('alert-danger').remove()
+    document.querySelector('.alert').remove()
 }
 
 loanForm.addEventListener('submit', e => {
