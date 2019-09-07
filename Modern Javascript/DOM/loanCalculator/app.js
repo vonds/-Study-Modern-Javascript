050c38f74d8dbbebf56bc2ec01fe035e59b77bfd
Vonds <vonds13@gmail.com>
2019-09-07 15:08:55 -0400

    loan calculator study 8

--- Modern Javascript/DOM/loanCalculator/app.js
+++ Modern Javascript/DOM/loanCalculator/app.js
@@ -1,32 +1,10 @@
 const loanForm = document.querySelector('#loan-form')
 
-const calculateResults = () => {
-    const amount = document.querySelector('#amount')
-    const interest = document.querySelector('#interest')
-    const years = document.querySelector('#years')
-    const monthlyPayment = document.querySelector('#monthly-payment')
-    const totalPayment = document.querySelector('#total-payment')
-    const totalInterest = document.querySelector('#total-interest')
-
-    const principal = parseFloat(amount.value)
-    const calculatedInterest = parseFloat(interest.value) / 100 / 12
-    const calculatedPayment = parseFloat(years.value) * 12
-
-    const x = Math.pow(1 + calculatedInterest, calculatedPayment)
-    const monthly = (principal * x * calculatedInterest) / (x - 1)
-
-    if (isFinite(monthly)) {
-        monthlyPayment.value = monthly.toFixed(2)
-        totalPayment.value = (monthly * calculatedPayment).toFixed(2)
-        totalInterest.value = ((monthly * calculatedPayment) - principal).toFixed(2)
-        document.querySelector('#results').style.display = 'block'
-        document.querySelector('#loading').style.display = 'none'
-    } else {
-        errorMessage('Please check your numbers')
-    }
+const clearError = () => {
+    document.querySelector('.alert-danger').remove()
 }
 
-const errorMessage = error => {
+const showError = error => {
     const errorDiv = document.createElement('div')
     const card = document.querySelector('.card')
     const heading = document.querySelector('.heading')
@@ -36,13 +14,37 @@ const errorMessage = error => {
     setTimeout(clearError, 2000)
 }
 
-const clearError = () => {
-    document.querySelector('.alert').remove()
+const calculatedResults = () => {
+    const amount = document.querySelector('#amount')
+    const interest = document.querySelector('#interest')
+    const years = document.querySelector('#years')
+    const monthlyPayment = document.querySelector('#monthly-payment')
+    const totalPayment = document.querySelector('#total-payment')
+    const totalInterest = document.querySelector('#total-interest')
+
+    const principal = parseFloat(amount.value)
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12
+    const calculatedPayments = parseFloat(years.value) * 12
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+    const monthly = (principal * x * calculatedInterest) / (x - 1)
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2)
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2)
+        totalInterest.value = ((monthly * calculatedPayments) - principal)
+        document.querySelector('#results').style.display = 'block'
+        document.querySelector('#loading').style.display = 'none'
+    } else {
+        document.querySelector('#results').style.display = 'none'
+        document.querySelector('#loading').style.display = 'none'
+        showError('Please check your numbers')
+    }
 }
 
 loanForm.addEventListener('submit', e => {
     document.querySelector('#results').style.display = 'none'
     document.querySelector('#loading').style.display = 'block'
-    setTimeout(calculateResults, 5000)
+    setTimeout(calculatedResults, 5000)
     e.preventDefault()
 })
