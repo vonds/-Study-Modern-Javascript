6959f5ffaa041c3f0ed0105463737e4d22b6452c
Vonds <vonds13@gmail.com>
2019-09-11 08:53:24 -0400

    async await study

--- Modern Javascript/ASYNC/easyhttp/scrap.js
+++ Modern Javascript/ASYNC/easyhttp/scrap.js
@@ -1,1 +1,10 @@
 //https://jsonplaceholder.typicode.com/todos/1
+
+const getTodos = async url => {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+getTodos('https://jsonplaceholder.typicode.com/todos')
+    .then(data => console.log(data))
