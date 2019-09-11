4d609ec24efb5b13555c3c3596d88dd84f7f0fe9
Vonds <vonds13@gmail.com>
2019-09-11 09:17:03 -0400

    async await 2 study

--- Modern Javascript/ASYNC/easyhttp/app.js
+++ Modern Javascript/ASYNC/easyhttp/app.js
@@ -10,18 +10,18 @@ const data2 = {
 }
 
 http.put('https://jsonplaceholder.typicode.com/todos/1', data2)
-    .then(data => console.log(data))
+    .then(data => console.log('PUT: ', data))
     .catch(err => console.log(err))
 
 
-http.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(data => console.log(data))
+http.get('https://jsonplaceholder.typicode.com/todos')
+    .then(data => console.log('GET: ', data))
     .catch(err => console.log(err))
 
 http.post('https://jsonplaceholder.typicode.com/todos', data)
-    .then(data => console.log(data))
+    .then(data => console.log('POST: ', data))
     .catch(err => console.log(err))
 
 http.delete('https://jsonplaceholder.typicode.com/todos/1')
-    .then(data => console.log(data))
+    .then(data => console.log('DELETE: ', data))
     .catch(err => console.log(err))
