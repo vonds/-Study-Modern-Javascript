8805334958db6df2331788214b98db9e94bdabcd
Vonds <vonds13@gmail.com>
2019-09-17 08:45:21 -0400

    json study 4

--- Modern Javascript/ASYNC/json/app.js
+++ Modern Javascript/ASYNC/json/app.js
@@ -1,15 +1,15 @@
 const loadCustomer = () => {
     const xhr = new XMLHttpRequest()
     xhr.open('GET', 'customer.json', true)
-    xhr.onload = function() {
-        if (this.status === 200) {
-            const customers = JSON.parse(this.responseText)
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            const customer = JSON.parse(xhr.responseText)
             const output = `
                 <ul>
-                    <li>ID: ${customers.id}</li>
-                    <li>Name: ${customers.name}</li>
-                    <li>Age: ${customers.age}</li>
-                    <li>Company: ${customers.company}</li>
+                    <li>ID: ${customer.id}</li>
+                    <li>Name: ${customer.name}</li>
+                    <li>Age: ${customer.age}</li>
+                    <li>Company: ${customer.company}</li>
                 </ul>
             `
             document.querySelector('#customers').innerHTML = output
@@ -21,10 +21,10 @@ const loadCustomer = () => {
 const loadCustomers = () => {
     const xhr = new XMLHttpRequest()
     xhr.open('GET', 'customers.json', true)
-    xhr.onload = function() {
-        if (this.status === 200) {
-            const customers = JSON.parse(this.responseText)
+    xhr.onload = () => {
+        if (xhr.status === 200) {
             let output
+            const customers = JSON.parse(xhr.responseText)
             customers.forEach(customer => {
                 output += `
                 <ul>
@@ -35,7 +35,7 @@ const loadCustomers = () => {
                 </ul>
             `
             })
-            document.querySelector('#customers').innerHTML = output
+            return document.querySelector('#customers').innerHTML = output
         }
     }
     xhr.send()
