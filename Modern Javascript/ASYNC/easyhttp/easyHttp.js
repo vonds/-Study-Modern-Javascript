598db4cc0fd5d186d3bf4e782312b06f1053ced8
Vonds <vonds13@gmail.com>
2019-09-17 13:01:12 -0400

    easyHttp study 5

--- Modern Javascript/ASYNC/easyhttp/easyHttp.js
+++ Modern Javascript/ASYNC/easyhttp/easyHttp.js
@@ -1,15 +1,15 @@
-function EasyHttp() {
+function easyHttp() {
     this.http = new XMLHttpRequest()
 }
 
-EasyHttp.prototype.get = (url, callback) => {
+easyHttp.prototype.get = (url, callback) => {
     const http = this.http
     http.open('GET', url, true)
     http.onload = () => {
         if (http.status === 200) {
             callback(null, http.responseText)
         } else {
-            callback(`Error ${http.status}`)
+            callback(`Error ${http.responseText}`)
         }
     }
     http.send()
