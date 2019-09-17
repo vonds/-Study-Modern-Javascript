644f4d05bfccd8828fd0b8b5cb0433c4ae9120e1
Vonds <vonds13@gmail.com>
2019-09-17 14:12:04 -0400

    easyhttp study 6

--- Modern Javascript/ASYNC/easyhttp/easyHttp.js
+++ Modern Javascript/ASYNC/easyhttp/easyHttp.js
@@ -14,3 +14,66 @@ easyHttp.prototype.get = (url, callback) => {
     }
     http.send()
 }
+
+easyHttp.prototype.post = (url, data, callback) => {
+    const http = this.http
+    http.open('POST', url, true)
+    http.setRequestHeader('Content-type', 'application/json')
+    http.onload = () => {
+        callback(null, http.responseText)
+    }
+    http.send(JSON.stringify(data))
+}
+
+easyHttp.prototype.put = (url, data, callback) => {
+    const http = this.http
+    http.open('PUT', url, true)
+    http.setRequestHeader('Content-type', 'application/json')
+    http.onload = () => {
+        callback(null, http.responseText)
+    }
+    http.send(JSON.stringify(data))
+}
+
+easyHttp.prototype.delet = (url, callback) => {
+    const http = this.http
+    http.open('DELETE', url, true)
+    http.setResquestHeader('Content-type', 'application/json')
+    http.onload = () => {
+        if (http.status === 200) {
+            callback(null, {})
+        } else {
+            callback(`Error ${http.responseText}`)
+        }
+
+    }
+    http.send()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// easyHttp.prototype.post = (url, data, callback) => {
+//     const http = this.http
+//     http.open('POST', url, true)
+//     http.setRequestHeader('Content-type', 'application/json')
+//     http.onload = () => {
+//         callback(null, http.responseText)
+//     }
+//     http.send(JSON.stringify(data))
+// }
+
