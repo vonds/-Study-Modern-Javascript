4d813047e47427c3ebe5066e5182f1ace6748c20
Vonds <vonds13@gmail.com>
2019-09-22 16:32:12 -0400

    modules study 2

--- Modern Javascript/PATTERNS/modules/app.js
+++ Modern Javascript/PATTERNS/modules/app.js
@@ -8,18 +8,87 @@
 // })()
 
 // Standard module pattern
-const UICtrl = (function () {
-    let text = 'Hello Squirrel'
+// const UICtrl = (function () {
+//     let text = 'Hello Squirrel'
 
-    const changeText = () => {
-        const element = document.querySelector('h1')
-        element.textContent = text
+//     const changeText = () => {
+//         const element = document.querySelector('h1')
+//         element.textContent = text
+//     }
+
+//     return {
+//         callChangeText: () => {
+//             changeText()
+//             console.log(text)
+//         }
+//     }
+// })()
+
+// Reveling Module Pattern
+const ItemCtrl = (function () {
+    let data = []
+
+    function add(item) {
+        data.push(item)
+        console.log(`${item.name} added...`)
+    }
+
+    function get(id) {
+        return data.find(item => {
+            return item.id === id
+        })
     }
 
     return {
-        callChangeText: () => {
-            changeText()
-            console.log(text)
-        }
+        add: add,
+        get: get
     }
 })()
+
+// 1
+const ItemCtrl = (function () {
+
+})()
+
+// 2
+const ItemCtrl = (function () {
+
+})()
+
+// 3
+const ItemCtrl = (function () {
+
+})()
+
+// 4
+const ItemCtrl = (function () {
+
+})()
+
+// 5
+const ItemCtrl = (function () {
+
+})()
+
+// 6
+const ItemCtrl = (function () {
+
+})()
+
+// 7
+const ItemCtrl = (function () {
+
+})
+
+// 8
+
+// 9
+
+// 10
+
+ItemCtrl.add({ id: 1, name: 'Soup' })
+ItemCtrl.add({ id: 2, name: 'Cucumbers' })
+ItemCtrl.add({ id: 3, name: 'Mangos' })
+ItemCtrl.add({ id: 4, name: 'Pineapples' })
+console.log(ItemCtrl.get(4))
+
