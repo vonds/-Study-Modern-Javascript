544f7474c33db20ed642d7f863f6b2940e128520
Vonds <vonds13@gmail.com>
2019-10-10 10:47:05 -0400

    JS Basics study 4

--- JS Course/complete-javascript-course/2-JS-basics/starter/app.js
+++ JS Course/complete-javascript-course/2-JS-basics/starter/app.js
@@ -23,23 +23,47 @@
 // } else {
 //     console.log('There is a tie')
 // }
-function tipCalculator(arr) {
-    let tips = []
-    let totals = []
-    for (let i = 0; i <= arr.length; i++) {
-        if (arr[i] < 50) {
-            tips.push(arr[i] * .2)
-            totals.push(arr[i] * .2 + arr[i])
-        } else if (arr[i] >= 50 && arr[i] <= 200) {
-            tips.push(arr[i] * .15)
-            totals.push(arr[i] * .15 + arr[i])
-        } else if (arr[i] > 200) {
-            tips.push(arr[i] * .1)
-            totals.push(arr[i] * .1 + arr[i])
+// function tipCalculator(arr) {
+//     let tips = []
+//     let totals = []
+//     for (let i = 0; i <= arr.length; i++) {
+//         if (arr[i] < 50) {
+//             tips.push(arr[i] * .2)
+//             totals.push(arr[i] * .2 + arr[i])
+//         } else if (arr[i] >= 50 && arr[i] <= 200) {
+//             tips.push(arr[i] * .15)
+//             totals.push(arr[i] * .15 + arr[i])
+//         } else if (arr[i] > 200) {
+//             tips.push(arr[i] * .1)
+//             totals.push(arr[i] * .1 + arr[i])
 
-        }
-    }
-    console.log('this is the tips array', tips)
-    console.log('this is the totals array', totals)
+//         }
+//     }
+//     console.log('this is the tips array', tips)
+//     console.log('this is the totals array', totals)
+// }
+// tipCalculator([124, 48, 268])
+
+
+const john = {
+    name: 'John',
+    height: 1.77,
+    mass: 81.6
 }
-tipCalculator([124, 48, 268])
+
+const mark = {
+    name: 'Mark',
+    height: 1.77,
+    mass: 81.6
+}
+
+const johnBMI = john.mass / (john.height * john.height)
+const markBMI = mark.mass / (mark.height * mark.height)
+
+if (johnBMI < markBMI) {
+    console.log("Is mark's BMI higher than john's? True")
+} else if (johnBMI > markBMI) {
+    console.log("Is mark's BMI higher than john's? False")
+} else {
+    console.log('they have the same BMI')
+}
