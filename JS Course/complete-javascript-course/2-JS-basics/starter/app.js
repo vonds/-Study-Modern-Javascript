53a79a92c61bd6efee233884e819c49f084e5a2b
Vonds <vonds13@gmail.com>
2019-10-10 10:12:42 -0400

    JS basic study 2

--- JS Course/complete-javascript-course/2-JS-basics/starter/app.js
+++ JS Course/complete-javascript-course/2-JS-basics/starter/app.js
@@ -1,11 +1,25 @@
 
-const johnHeight = 175
-const markHeight = 187
-const johnWeight = 68
-const markWeight = 77
+// const johnHeight = 175
+// const markHeight = 187
+// const johnWeight = 68
+// const markWeight = 77
 
-if ((johnHeight * johnHeight) / johnWeight < (markHeight & markHeight) / markWeight) {
-    console.log("Is mark's BMI higher than john's? True")
+// if ((johnHeight * johnHeight) / johnWeight < (markHeight & markHeight) / markWeight) {
+//     console.log("Is mark's BMI higher than john's? True")
+// } else {
+//     console.log("Is mark's BMI higher than john's? False")
+// }
+
+const johnAverage = (97 + 134 + 105) / 3
+const mikeAverage = (116 + 94 + 123) / 3
+const maryAverage = (97 + 134 + 105) / 3
+
+if (johnAverage > mikeAverage && johnAverage > maryAverage) {
+    console.log(`John is the winner with an average score of: ${johnAverage}`)
+} else if (mikeAverage > johnAverage && mikeAverage > maryAverage) {
+    console.log(`Mike is the winner with an average score of: ${mikeAverage}`)
+} else if (maryAverage > johnAverage && maryAverage > mikeAverage) {
+    console.log(`Mary is the winner with an average score of: ${maryAverage}`)
 } else {
-    console.log("Is mark's BMI higher than john's? False")
+    console.log('There is a tie')
 }
