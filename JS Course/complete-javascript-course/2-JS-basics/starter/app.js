7ce499b7944523e061404db622cfc699e1da12e1
Vonds <vonds13@gmail.com>
2019-10-10 10:00:48 -0400

    coding challenge study

---
+++ JS Course/complete-javascript-course/2-JS-basics/starter/app.js
@@ -0,0 +1,11 @@
+
+const johnHeight = 175
+const markHeight = 187
+const johnWeight = 68
+const markWeight = 77
+
+if ((johnHeight * johnHeight) / johnWeight < (markHeight & markHeight) / markWeight) {
+    console.log("Is mark's BMI higher than john's? True")
+} else {
+    console.log("Is mark's BMI higher than john's? False")
+}
